6894dbd2a7b06fa0a0d5970346643088abd44f19
Remi Huigen <remihuigen@proton.me>
2026-07-11 20:00:28 +0200

    feat: Update docs and add oxfmt"

---
+++ oxfmt.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  trailingComma: "none",
+  ignorePatterns: [
+    ".idea/",
+    ".next/",
+    "pnpm-lock.yaml",
+    "CHANGELOG.md",
+    "dist",
+    ".nuxt",
+    "nuxt/.nuxt",
+    ".output",
+    "node_modules",
+    "content/",
+    ".agent/skills",
+    ".agents/skills",
+    ".agents/**",
+    "scripts/**/*.sh",
+    "envs/env.d.ts",
+  ],
+});
